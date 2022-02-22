a556f726f0f373ab38faba9647733b3f34cee94d
Josephine237 <josefina.novak@gmail.com>
2022-02-22 09:50:36 +0100

    Přidány poznámky z hodiny

--- 05 - Drobnosti/index.js
+++ 05 - Drobnosti/index.js
@@ -1,8 +1,32 @@
 'use strict';
 console.log('%c JavaScript funguje! ', 'background-color: black; color: lime;');
 
-/*
-- Template string Ahoj ${x} světe!
-- Ternární operátor
-*/
+
+// Template string Ahoj ${x} světe!
+
+    let firstName = "Josie";
+    let age = 23;
+
+    console.log("Ahoj, jmenuji se " + firstName + ", a je mi " + age + " let.");
+     
+    // pomocí teplate stringu, to lze napsat o dost jednodušejí ... píšeme ho pomocí zpětných uvozovek
+
+    console.log(`Ahoj, jmenuji se ${firstName}, a je mi ${age} let.`)
+
+// Ternární operátor
+
+    let favoritePhrase = "Love That";
+
+    if (favoritePhrase === "Love That") {
+        console.log("I Love That!");
+    } else {
+        console.log("I don't like That");
+    }
+
+        // toto se dá napsat pomocí ternárního operátoru
+        favoritePhrase === "Love That" ? console.log("I love That!") : console.log("I Don't Like That")
+
+    let userName = " "
+
+    userName ? console.log(`Hello, ${userName}!`) : console.log("Hello!");
 
